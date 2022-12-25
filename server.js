e1bd0ddc04ddd0fbab9a8875b602a7302cf95ac4
Lehi Cano <onacihel@gmail.com>
2022-12-25 13:45:17 -0700

    wip sets up env

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 // Connect to Mongo &
 // Fix Depreciation Warnings from Mongoose
 // May or may not need these depending on your Mongoose version
-mongoose.connect(MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true }
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
 // Error / success
@@ -40,7 +40,7 @@ db.on('disconnected', () => console.log('mongod disconnected'));
 app.use(express.static('public'));
 
 // populates req.body with parsed info from forms - if no data from forms will return an empty object {}
-app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings
+app.use(express.urlencoded({ extended: true }));// extended: false - does not allow nested objects in query strings
 app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
 
 //use method override
