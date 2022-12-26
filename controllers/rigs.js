397a414dd4ca68ec7d40b0d04cc349a6902b2cc3
Lehi Cano <onacihel@gmail.com>
2022-12-26 12:14:20 -0700

    adds working header partials

--- controllers/rigs.js
+++ controllers/rigs.js
@@ -4,5 +4,8 @@ const router = express.Router()
 router.get('/', (req,res) => {
     res.render('rigs/index.ejs')
 })
+router.get('/new', (req,res) => {
+    res.render('rigs/new.ejs')
+})
 
 module.exports = router
